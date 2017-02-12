8eb2ecc8783efe39195dde12cac13ed8b30e6c4c
Mikhail Vasiliev <Mikhail@Mikhails-iMac.local>
2017-02-12 18:41:23 +0200

    Init

---
+++ App/Navigation/NavigationRouter.js
@@ -0,0 +1,26 @@
+// @flow
+
+import React, { Component } from 'react'
+import { Scene, Router } from 'react-native-router-flux'
+import Styles from './Styles/NavigationContainerStyle'
+
+// screens identified by the router
+import PresentationScreen from '../Containers/PresentationScreen'
+
+/* **************************
+* Documentation: https://github.com/aksonov/react-native-router-flux
+***************************/
+
+class NavigationRouter extends Component {
+  render () {
+    return (
+      <Router>
+          <Scene key='drawerChildrenWrapper' navigationBarStyle={Styles.navBar} titleStyle={Styles.title} leftButtonIconStyle={Styles.leftButton} rightButtonTextStyle={Styles.rightButton}>
+            <Scene initial key='presentationScreen' component={PresentationScreen} title='Ignite' />
+          </Scene>
+      </Router>
+    )
+  }
+}
+
+export default NavigationRouter
---
+++ App/Sagas/index.js
@@ -0,0 +1,29 @@
+import { takeLatest } from 'redux-saga'
+import API from '../Services/Api'
+import DebugSettings from '../Config/DebugSettings'
+
+/* ------------- Types ------------- */
+
+import { StartupTypes } from '../Redux/StartupRedux'
+import { LoginTypes } from '../Redux/LoginRedux'
+
+/* ------------- Sagas ------------- */
+
+import { startup } from './StartupSagas'
+import { login } from './LoginSagas'
+
+/* ------------- API ------------- */
+
+// The API we use is only used from Sagas, so we create it here and pass along
+// to the sagas which need it.
+const api = API.create()
+
+/* ------------- Connect Types To Sagas ------------- */
+
+export default function * root () {
+  yield [
+    // some sagas only receive an action
+    takeLatest(StartupTypes.STARTUP, startup),
+    takeLatest(LoginTypes.LOGIN_REQUEST, login),
+  ]
+}
