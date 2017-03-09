e306e48ad278925209419b74d12bb71ae3eebabb
MikhailVasiliev <mr.m.vasiliev@gmail.com>
2017-03-09 22:11:26 +0200

    nav drawer

--- App/Containers/PresentationScreen.js
+++ App/Containers/PresentationScreen.js
@@ -2,16 +2,20 @@
 
 import React from 'react'
 import { ScrollView, Text, View, ListView, TouchableOpacity } from 'react-native'
-import { Images, Metrics } from '../Themes'
+import { Images, Metrics, Colors } from '../Themes'
 import { Actions as NavigationActions } from 'react-native-router-flux'
 import ArticlesActions from '../Redux/ArticlesRedux'
 
 // Components
 import SwiperItem from '../Components/SwiperItem'
 
+import ArticleScreen from './ArticleScreen'
+
 // External libs
 import Image from 'react-native-image-progress';
 import Swiper from 'react-native-swiper';
+import Icon from 'react-native-vector-icons/EvilIcons'
+import {OffCanvas3D} from 'react-native-off-canvas-menu'
 
 // Redux
 import { connect } from 'react-redux'
@@ -21,9 +25,39 @@ import styles from './Styles/PresentationScreenStyles'
 
 class PresentationScreen extends React.Component {
 
+  constructor () {
+    super()
+
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  componentDidMount() {
+    NavigationActions.refresh({onLeft: this.handleMenu.bind(this)})
+  }
+
   render () {
     return (
       <View style={styles.main}>
+        <OffCanvas3D
+          active={this.state.menuOpen}
+          onMenuPress={this.handleMenu.bind(this)}
+          backgroundColor={'#222222'}
+          menuTextStyles={{color: 'white', marginTop: 40, backgroundColor: Colors.transparent}}
+          handleBackPress={true}
+          menuItems={[
+            {
+              title: 'Menu 1',
+              icon: <Icon name="camera" size={35} color="#ffffff" style={{marginTop: 40}} />,
+              renderScene: <SwiperItem/>
+            },
+            {
+              title: 'ArticleScreen',
+              icon: <Icon name="bell" size={35} color="#ffffff" style={{marginTop: 40}}/>,
+              renderScene: <ArticleScreen/>
+            }
+          ]}>
         <Swiper horizontal={false}  >
           <SwiperItem/>
           <SwiperItem/>
@@ -38,8 +72,16 @@ class PresentationScreen extends React.Component {
             <Text style={styles.footerButtonText}>Next</Text>
           </TouchableOpacity>
         </View>
+        </OffCanvas3D>
       </View>
     )
+  }
+
+  handleMenu() {
+    const {menuOpen} = this.state
+    this.setState({
+      menuOpen: !menuOpen
+    })
   }
 }
 
